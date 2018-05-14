b1f7585b477aa4fa08d6be4100225bad83eb9db3
Ahmad Zubair Faras <zubairfaras1@gmail.com>
2018-05-13 23:56:27 -0700

    created table and linked it to script and tested it.

--- smart-taxi-meter-app/app/scripts/script.js
+++ smart-taxi-meter-app/app/scripts/script.js
@@ -5,17 +5,17 @@ var fnameInput = document.getElementById('fname-input');
 var lnameInput = document.getElementById('lname-input');
 var emailInput = document.getElementById('email-input');
 var mileageInput = document.getElementById('mileage-input');
-var cost1Table = document.getElementById('cost1-table');
-var mile1Table = document.getElementById('mile1-table');
-var mile2Table = document.getElementById('mile2-table');
-var cost2Table = document.getElementById('cost2-table').innerHTML = mile2Table.value;
+var cost1TableInput = document.getElementById('cost1-table-input');
+var mile1TableInput = document.getElementById('mile1-table-input');
+var mile2TableInput = document.getElementById('mile2-table-input');
+var cost2TableInput = document.getElementById('cost2-table-input');
 var outputP = document.getElementById('output-p')
 
 taxiForm.addEventListener('submit', function(){
 	event.preventDefault();
 
-	outputP.innerHTML = 'This ride costs you $' + mileageInput.value * 2;
-	cost1Table.innerHTML = mileageInput.value * 2;
-	//cost2Table.innerHTML = mile2Table * 2;
+	outputP.innerHTML = 'This ride costs you $' + mileageInput.value * 2 + '.';
+	cost1TableInput.innerHTML = 2 * 2;
+	cost2TableInput.innerHTML = 4 * 2;
 });
 
