10f30729d5f9efcfbb2baf17dbe191f6102bf9f8
Ahmad Zubair Faras <zubairfaras1@gmail.com>
2018-05-18 17:56:56 -0700

    Created GET request.

--- smart-taxi-meter-app/app/scripts/script.js
+++ smart-taxi-meter-app/app/scripts/script.js
@@ -1,15 +1,54 @@
 'use strict'
 
+// We create taxiApp module in script.js
+var taxiApp = angular.module('taxiApp', []);
+
 var taxiForm = document.getElementById('taxi-form');
 var fnameInput = document.getElementById('fname-input');
 var lnameInput = document.getElementById('lname-input');
 var emailInput = document.getElementById('email-input');
 var mileageInput = document.getElementById('mileage-input');
 var ridersTbody = document.getElementById('riders-tbody');
-var outputP = document.getElementById('output-p')
+var outputP = document.getElementById('output-p');
+
+taxiApp.controller('mainController', function($scope, $http){
+	var getRiders = function(){
+//Retrieve riders data from server.'/riderList' is the route we'll get data from
+		//$http is angular object
+		$http.get('riderList').then(function(response){
+			var riders = response.data;
+			//$scope.movies = response.data;
+			for (var i = 0; i < riders.length; i++) {
+				ridersTbody.innerHTML += '<tr>'+
+									'<td>'+ riders[i].fname+'</td>'+
+									'<td>'+ riders[i].lname+'</td>'+
+									'<td>'+ riders[i].email+'</td>'+
+									'<td>'+ riders[i].mileage+'</td>'+
+									'<td>'+ riders[i].cost+'</td>'+
+								'</tr>'	
+}
+			
+	})
+
+}
+
+getRiders();
+
+
 
 taxiForm.addEventListener('submit', function(){
 	event.preventDefault();
+
+	function Rider (fname, lname, email, mileage, cost) {
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.mileage = mileage;
+		this.cost = cost;
+
+	}
+	var newRider = new Rider(fnameInput.value, lnameInput.value, emailInput.value, mileageInput.value, mileageInput.value*2);
+
 	ridersTbody.innerHTML += '<tr>'+
 									'<td>'+fnameInput.value+'</td>'+
 									'<td>'+lnameInput.value+'</td>'+
@@ -18,6 +57,7 @@ taxiForm.addEventListener('submit', function(){
 									'<td>'+mileageInput.value * 2 +'</td>'+
 								'</tr>'
 	outputP.innerHTML = 'This ride costs you $'+mileageInput.value*2 + '.';
+
 	// After taking data from input, we assign empty string to them to 
 	// make input field clear or clean.
 	
@@ -28,7 +68,9 @@ taxiForm.addEventListener('submit', function(){
 
 	
 
-});
+})
+})
+
 
 /*
 var ahmadFaras = {
