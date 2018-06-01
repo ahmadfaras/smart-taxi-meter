1556dc0f3a723860704a67badf90cf3d79be9e90
Ahmad Zubair Faras <zubairfaras1@gmail.com>
2018-05-31 18:27:42 -0700

    commented out the delete command

--- smart-taxi-meter-app/server.js
+++ smart-taxi-meter-app/server.js
@@ -11,35 +11,12 @@ app.use(express.static(__dirname + '/app/'));
 
 app.get('/riderList', function(request, response){
 	
-	/* Now server responds to GET with objects from db, so we commented out these objects.
-	var ahmadFaras = {
-	fname: 'Ahmad',
-	lname: 'Faras',
-	email: 'zubairfaras1@gmail.com',
-	mileage: 2,
-	cost: 2*2
-};
-var sunShayina = {
-	fname: 'Sun',
-	lname: 'Shayina',
-	email: 'shayinamawezi@gmail.com',
-	mileage: 4,
-	cost: 4*2
-};
-
-var riderList = [ahmadFaras, sunShayina];
-*/
-
-
 db.taxidb.find(function(error, docs){
 		console.log(docs);
 		response.json(docs);
 	})
 
 console.log('server received GET request');
-//console.log(riderList);
-
- 
 })
 
 app.post('/riderList', function(request, response){
@@ -48,12 +25,12 @@ app.post('/riderList', function(request, response){
 		response.json(docs);
 	});
 
-	$scope.deleteRider = function(id){
-	console.log(id);
-	$http.delete('/riderList/' + id).then(function(response){
-		getRiders();
-	});
-}
+// 	$scope.deleteRider = function(id){
+// 	console.log(id);
+// 	$http.delete('/riderList/' + id).then(function(response){
+// 		getRiders();
+// 	});
+// }
 
 
 });
